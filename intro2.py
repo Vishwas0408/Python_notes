baa694a41c416e099c88c86665026dc9e6491fb3
Vishwas0408 <168621223+Vishwas0408@users.noreply.github.com>
2024-05-01 11:45:06 +0530

    Add files via upload

---
+++ intro2.py
@@ -0,0 +1,173 @@
+#print ("hain vaiii kidaan")
+#print ("faad veere faad")
+# print("eee vedeyaaaa")
+#-----------------------------------------------------------------
+
+#Introduction
+
+
+# variable :- It is a short name of any value OR it is a container where we store multiple values in a single variable.
+
+#x = "welcome to python"
+#print (x)
+
+# x = "hello world"
+# print (x)
+
+# comments :- it is a statement that we want to show but dont want to be executed.
+
+# there are two types of statement 
+# 1 single statement(#) 
+# 2 multiline statement('''  ''') or ("""  """)
+
+#x,y,z=10,20,30
+#print(x,y,z)
+
+"""
+rotihjthet
+detgh
+gth
+etd
+hd
+hghh
+"""
+
+'''
+* Limitations of variables 
+1. it should be written in alphanumeric charactor
+2. it should be start with alphabets
+3. it cannot be written in number
+4. you cannot take these symbols for variable like (!@#$%^&*)
+5. variables are case sensitive
+'''
+
+# indentation error :- when extra space is given before code
+
+#x = 45,34,23
+#print(x)
+
+'''
+* Data type in python
+1. Numerical data:- int,float,complex
+2. text data:- string str
+3. sequence:- list,tuple,range
+4. mapping:- dictionary
+5. boolean:- True , False
+6. binary:- 01010101010101
+7. none:- none
+'''
+
+# 1. NUMERICAL data
+'''
+1. integer int (4,5,3,7,2,4)
+2. float (45.23,2.0,1,6)
+3. complex 21j 45j 87j
+'''
+# 2. TEXT data 
+'''
+string str " " or ' '
+it is always written in single or double quotation.
+ 
+ x="vishwas"
+print(type(x))
+
+x="345"
+print(type(x))
+
+x="data science"
+y=45j
+z=[23,45,34]
+a=(12,34,76)
+b={34,6,8}
+c="23data"
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
+print(type(c))
+'''
+#x= "2"
+#y= "2"
+#print(x+y)
+
+
+# 3. SEQUENCE DATA
+#List:- [3,4,5,"data",true]
+#tuple:-(45,2,6,2,5)
+#range:- range()
+
+# 4. MAPPING DATA
+# DICTIONARRY:-  DICT  - {key : value}
+# "laptop" : -------------------------------------
+
+
+# HOW TO CHECK LENGTH OF A TEXT
+
+# x= "DATA ANALYST"
+# print(len(x))
+# y= len(x)
+# print(y)
+
+# x="456"
+# y=len(x)
+# print(y)
+
+
+# OPERATORS:-
+
+# ARTHEMATIC OPERATOR:- [+ , - , * , / , % mod , ** exponent , // floor]
+
+# write a python to add these 3 numbers
+
+x=45
+y=10
+z=5
+
+a= x+y+z
+print("Addition:-",a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
